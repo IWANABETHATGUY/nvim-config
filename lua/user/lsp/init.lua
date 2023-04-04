728ded125dbb2cd5fc626abff2d3a729391d8206
IWANABETHATGUY <iwanabethatguy@qq.com>
2023-04-04 22:17:45 +0800

    feat: enable rustHoverAction

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -6,7 +6,7 @@ end
 require "user.lsp.configs"
 require("user.lsp.handlers").setup()
 require("user.lsp.settings.tjs_language_server")
-require "user.lsp.null-ls"
+-- require "user.lsp.null-ls"
 -- require('lspconfig').tjs_language_server.setup()
 -- require('lspconfig').rust_analyzer.setup()
 
--- lua/user/rust-tools.lua
+++ lua/user/rust-tools.lua
@@ -2,13 +2,15 @@ local rt = require("rust-tools")
 local handler = require("user.lsp.handlers")
 
 rt.setup({
+  tools = {
+    hover_actions = {
+      auto_focus = true
+    }
+  },
   server = {
-    on_attach = handler.on_attach
-    -- on_attach = function(_, bufnr)
+    on_attach = handler.on_attach,
+    -- on_attach = function(client, bufnr)
     --   -- Hover actions
-    --   vim.keymap.set("n", "gh", rt.hover_actions.hover_actions, { buffer = bufnr })
-    --   -- Code action groups
-    --   vim.keymap.set("n", "<leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
     -- end,
   },
 })
