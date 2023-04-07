64246be7ddb52441d6adb7c4c088a29bc3beee65
IWANABETHATGUY <iwanabethatguy@qq.com>
2023-04-07 21:37:36 +0800

    feat: fterm

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -1,30 +1,37 @@
-local status_ok, toggleterm = pcall(require, "toggleterm")
-if not status_ok then
-  return
-end
-
-toggleterm.setup({
-  size = 30,
-  open_mapping = [[<c-\>]],
-  hide_numbers = true,
-  shade_filetypes = {},
-  shade_terminals = true,
-  shading_factor = 2,
-  start_in_insert = true,
-  insert_mappings = true,
-  persist_size = true,
-  direction = "horizontal",
-  close_on_exit = true,
-  shell = vim.o.shell,
-  float_opts = {
-    border = "curved",
-    winblend = 0,
-    highlights = {
-      border = "Normal",
-      background = "Normal",
+require'FTerm'.setup({
+    border = 'double',
+    dimensions  = {
+        height = 0.9,
+        width = 0.9,
     },
-  },
 })
+
+-- Example keybindings
+vim.keymap.set('n', '<c-\\>', '<CMD>lua require("FTerm").toggle()<CR>')
+vim.keymap.set('t', '<c-\\>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+
+-- toggleterm.setup({
+--   size = 30,
+--   open_mapping = [[<c-\>]],
+--   hide_numbers = true,
+--   shade_filetypes = {},
+--   shade_terminals = true,
+--   shading_factor = 2,
+--   start_in_insert = true,
+--   insert_mappings = true,
+--   persist_size = true,
+--   direction = "horizontal",
+--   close_on_exit = true,
+--   shell = vim.o.shell,
+--   float_opts = {
+--     border = "curved",
+--     winblend = 0,
+--     highlights = {
+--       border = "Normal",
+--       background = "Normal",
+--     },
+--   },
+-- })
 
 function _G.set_terminal_keymaps()
   local opts = { noremap = true }
@@ -38,33 +45,3 @@ end
 
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
-local Terminal = require("toggleterm.terminal").Terminal
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
-
-function _LAZYGIT_TOGGLE()
-  lazygit:toggle()
-end
-
-local node = Terminal:new({ cmd = "node", hidden = true })
-
-function _NODE_TOGGLE()
-  node:toggle()
-end
-
-local ncdu = Terminal:new({ cmd = "ncdu", hidden = true })
-
-function _NCDU_TOGGLE()
-  ncdu:toggle()
-end
-
-local htop = Terminal:new({ cmd = "htop", hidden = true })
-
-function _HTOP_TOGGLE()
-  htop:toggle()
-end
-
-local python = Terminal:new({ cmd = "python", hidden = true })
-
-function _PYTHON_TOGGLE()
-  python:toggle()
-end
